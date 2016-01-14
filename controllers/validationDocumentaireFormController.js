62133ff1c0f591db6f3dddad877a337f79c2b5e9
yann <yann.boisselier@gmail.com>
2016-01-14 11:03:16 +0100

    refactor enregistrementDemande

--- controllers/validationDocumentaireFormController.js
+++ controllers/validationDocumentaireFormController.js
@@ -6,7 +6,7 @@
 
   "use strict";
 
-  angular.module('validationDocumentaire').controller("validationDocumentaireFormController", ['validationDocumentaireService', 'userService', '$rootScope', 'validationDocumentaireSettings', '$scope', 'Upload', '$timeout', '$http', '$q',function(validationDocumentaireService, userService, $rootScope, validationDocumentaireSettings, $scope, Upload, $timeout, $http, $q) {
+  angular.module('validationDocumentaire').controller("validationDocumentaireFormController", ['validationDocumentaireService', 'userService', '$rootScope', 'validationDocumentaireSettings', '$scope', 'Upload', '$timeout', '$http', '$q', function(validationDocumentaireService, userService, $rootScope, validationDocumentaireSettings, $scope, Upload, $timeout, $http, $q) {
 
     // Le formulaire est composé de plusieurs "pages", par défaut mettre sur la première page.
     $scope.currentStep = 1;
@@ -71,10 +71,10 @@
      * récupérer tous les utilisateurs cochés depuis le formulaire dans les onglets entités, service et groupes de travail.
      * @returns {Array}
      */
-
     $scope.getAllSelectedUsers = function() {
       var selectedUsers = [];
       // juste un hack pour ne pas enregistrer deux fois le même utilisateur
+      // si un même utilisateur appartient à plusieurs groupes en même temps.
       var selectedUsersIds = [];
       // on itére sur les trois groupes possibles utilisateurs : service, entité, groupe de travail
       angular.forEach($scope.users, function(groupes) {
@@ -83,7 +83,6 @@
         angular.forEach(groupes.groups, function(groupe) {
           angular.forEach(groupe.users, function(user) {
             if (user.selected && selectedUsersIds.indexOf(user.id) === -1) {
-              //selectedUsers[user.id] = user;
               selectedUsersIds.push(user.id);
               selectedUsers.push(user);
             }
@@ -94,29 +93,25 @@
       return selectedUsers;
     };
 
-
     $scope.setPrioriteOptionSelected = function(option) {
       $scope.prioriteOptionSelected = option;
     };
 
     // Demander la liste des priorités existances au serveur via un webservice.
-    // demander au serveur la liste des priorités existantes :
-
-    //Ne marche : au retour de la promesse, on perd l'option par défaut !
     $scope.priorites = [];
     $http.get(validationDocumentaireSettings.webservicesBaseUrl + 'validationDocumentairePrioriteGetAll').then(function(response) {
       $scope.prioritesOptions = response.data.result;
       $scope.prioriteOptionSelected = $scope.prioritesOptions[1];
     });
 
-
     // permet d'afficher telle ou telle page du formulaire d'upload de fichiers
+    // qui est composé de plusieurs étapes
     $scope.goToStep = function(number) {
       $scope.currentStep = number;
     };
 
     // Garder en mémoire la liste des fichiers que l'utilisateur veut envoyer au serveur.
-    // Appellé au moment où l'utilisateur ajoute un fichier.
+    // Appellé au moment où l'utilisateur ajoute en drag and drop un fichier.
     $scope.addFileToQueue = function(files) {
       $scope.files = files;
     };
@@ -127,111 +122,49 @@
       $scope.files.splice(index, 1);
     };
 
+    // l'utilisateur actuellement connecté récupérer par webservice
     userService.getCurrentUser().then(function(currentUser) {
       $scope.currentUser = currentUser;
     });
 
-    // Uploader les fichiers sur le serveur puis créer en base
-    // la validation documentaire correspondant si l'upload est réussi
-    $scope.enregistrerDemande = function() {
 
-      var validationDocumentaireId = null;
-      var currentUserId = null;
-
-      // récupérer l'id de l'utilisateur courant
-      userService.getCurrentUserId()
-
-      // puis enregistrer en base sa demande
-      .then(function(userId) {
-          currentUserId = userId;
-          var datas = {};
-          datas.libelle = $scope.validationDocumentaire.libelle; // Ajout par Charlie le 23/12/2015
-          datas.description = $scope.validationDocumentaire.description;
-          datas.priorite_id = $scope.prioriteOptionSelected.id;
-          datas.date_cloture = $scope.validationDocumentaire.date_cloture;
-          datas.id_utilisateur = userId;
-          // insertion en base de données
-          return validationDocumentaireService.insert(datas);
-        })
-        // puis uploader les fichiers en créant un sous-repertoire nommé
-        // selon l'id de la validation documentaire précédemment créee
-        .then(function(lastInsertId) {
-          validationDocumentaireId = lastInsertId;
-          return uploadFiles($scope.files, lastInsertId);
-        })
-        // puis insérer les "destinataires" : les utilisateurs affiliés à cette demande
-        // ainsi que leur status de valideur.
-        .then(function() {
-
-          var selectedUsers = $scope.getAllSelectedUsers();
-
-          // on "cascade" toutes les promesses d'insertion pour en faire une promesse globale
-          // avec $q.all()
-          return $q.all(selectedUsers.map(function(user) {
-            var datas = {};
-            datas.id_validation_documentaire = validationDocumentaireId;
-            datas.id_utilisateur = user.id
-            datas.valideur = user.valideur == true ? 1 : 0;
-            return validationDocumentaireService.insertDestinataire(datas);
-          }));
-          /*
-          angular.forEach(selectedUsers, function(user) {
-            var datas = {};
-            datas.id_validation_documentaire = validationDocumentaireId;
-            datas.id_utilisateur = user.id
-            datas.valideur = user.valideur == true ? 1 : 0;
-            validationDocumentaireService.insertDestinataire(datas);
-          });
-           */
-
-        })
-        // Ajout le 23/12/2015
-        // Notification
-        .then(function() {
-
-          var datas = {};
-          datas.id = validationDocumentaireId;
-          datas.event = 'create';
-          return validationDocumentaireService.sendEmailToDestinaires(datas);
-
-        })
-
-      // quand l'upload est terminé :
-      .then(
-
-        // en cas de succès
-        function(response) {
-          // rediriger vers la page de confirmation de l'upload
-          $scope.goToStep(3);
-          // On lance un évènement comme quoi nous venons d'insérer une nouvelle validation en base.
-          $rootScope.$emit('validationDocumentaireAfterInsert');
-        },
-
-        // en cas d'erreur
-        function(response) {
-          if (response.status > 0) {
-            $scope.errorMsg = response.status + ': ' + response.data;
-          }
-        },
-
-        // callback de notification, permet d'afficher la progression
-        // de l'upload dans notre cas.
-        function(evt) {
-          $scope.progress =
-            Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-        }
-
-      );
-
-
+    // insérer la validation documentaire en base.
+    // retourne une promesse contenant le dernier id inséré
+    function validationDocumentaireInsert(userId) {
+      var datas = {};
+      datas.libelle = $scope.validationDocumentaire.libelle; // Ajout par Charlie le 23/12/2015
+      datas.description = $scope.validationDocumentaire.description;
+      datas.priorite_id = $scope.prioriteOptionSelected.id;
+      datas.date_cloture = $scope.validationDocumentaire.date_cloture;
+      datas.id_utilisateur = userId;
+      // insertion en base de données
+      return validationDocumentaireService.insert(datas);
     };
 
-    /**
-     * envoyer les fichiers "pour de vrai" sur le serveur, via le module ng-Upload.
-     * @return Promise
-     */
+    // Insérer tous les destinataires liés
+    // retourne une promesse
+    function destinatairesInsert(selectedUsers, validationDocumentaireId) {
+      return $q.all(selectedUsers.map(function(user) {
+        var datas = {};
+        datas.id_validation_documentaire = validationDocumentaireId;
+        datas.id_utilisateur = user.id
+        datas.valideur = user.valideur == true ? 1 : 0;
+        return validationDocumentaireService.insertDestinataire(datas);
+      }));
+    };
+
+    // Envoyer le mail à tous les destinataires
+    // retourne une promesse
+    function sendEmailToDestinaires(validationDocumentaireId) {
+      var datas = {};
+      datas.id = validationDocumentaireId;
+      datas.event = 'create';
+      return validationDocumentaireService.sendEmailToDestinaires(datas);
+    }
+
+    // envoyer les fichiers "pour de vrai" sur le serveur, via le module ng-Upload.
+    // @return Promise
     function uploadFiles(files, directory) {
-      // on envoie à notre action la liste des fichiers qu'on veut uploader
       return Upload.upload({
         url: validationDocumentaireSettings.webservicesBaseUrl + 'upload',
         data: {
@@ -242,6 +175,74 @@
         }
       });
     }
+
+    // Uploader les fichiers sur le serveur puis créer en base
+    // la validation documentaire correspondant si l'upload est réussi
+    $scope.enregistrerDemande = function() {
+
+      var validationDocumentaireId = null;
+
+      $scope.progressionEnregistrement = '';
+
+      // récupérer l'id de l'utilisateur courant
+      userService.getCurrentUserId()
+
+      // 1 - INSERTION VALIDATION DOCUMENTAIRE EN BASE
+      .then(function(userId) {
+        $scope.progressionEnregistrement = 'Insertion validation documentaire en base...';
+        return validationDocumentaireInsert(userId);
+      })
+
+      // 2 - INSERTION DES DESTINATAIRES EN BASE
+      .then(function(lastValidationDocumentaireId) {
+        $scope.progressionEnregistrement += 'Inserée avec id ' + lastValidationDocumentaireId + '. Insertion des destinataires en base ...';
+        // enregistrer l'id de validation pour s'en resservir plus bas.
+        validationDocumentaireId = lastValidationDocumentaireId
+        return destinatairesInsert($scope.getAllSelectedUsers(), validationDocumentaireId);
+      })
+
+      // 3 - UPLOAD DES FICHIERS SUR LE SERVEUR
+      .then(function(response) {
+        $scope.progressionEnregistrement += 'upload des fichiers en cours...';
+        return uploadFiles($scope.files, validationDocumentaireId);
+      })
+
+      // 4 - ENVOI DES EMAILS AUX DESTINATAIRES
+      .then(
+
+        // en cas de succès de l'upload des fichiers
+        function(response) {
+          $scope.progressionEnregistrement += 'Fichiers uploadés avec succès...';
+          $scope.progressionEnregistrement += 'Envoi des emails de notifications...';
+          return sendEmailToDestinaires(validationDocumentaireId);
+        },
+
+        // en cas d'erreur de l'upload
+        function(response) {
+          console.log(response);
+          if (response.status > 0) {
+            $scope.errorMsg = response.status + ': ' + response.data;
+          }
+        },
+
+        // notification de l'upload : affichage de la barre de progression d'upload des fichiers
+        function(evt) {
+          $scope.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+          $scope.progressionEnregistrement += $scope.progress;
+        }
+
+      )
+
+      // 5 - REDIRECTION VERS LA PAGE DE CONFIRMATION
+      .then(function(response) {
+        $scope.progressionEnregistrement += 'Succès.';
+        // rediriger vers la page de confirmation de la création de la validation documentaire
+        $scope.goToStep(3);
+        // On lance un évènement comme quoi nous venons d'insérer une nouvelle validation en base.
+        $rootScope.$emit('validationDocumentaireAfterInsert');
+      });
+
+    };
 
     $scope.getDatetime = new Date();
 
